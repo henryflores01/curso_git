45b85b09f6f4524f09e93e0c3d458705af746b95
Henry Flores <henry.flores2003@outlook.com>
2024-07-31 11:45:49 -0600

    Agregando todo a main()

--- calculadora.go
+++ calculadora.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func sumar(a, b int) int {
 	return a + b
 }
@@ -17,5 +19,13 @@ func dividir(a, b int) int {
 }
 
 func main() {
+	var a, b int
 
+	fmt.Println("Ingrese dos numeros: ")
+	fmt.Scanf("%d %d", &a, &b)
+
+	fmt.Println(sumar(a, b))
+	fmt.Println(restar(a, b))
+	fmt.Println(multiplicar(a, b))
+	fmt.Println(dividir(a, b))
 }
